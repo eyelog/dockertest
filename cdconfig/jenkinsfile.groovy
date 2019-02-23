3d8db51a19b1c80c3a2a1bcf63eea977f14d3c0e
eyelog <41166734+eyelog@users.noreply.github.com>
2019-02-24 02:01:09 +0300

    tap

--- cdconfig/jenkinsfile.groovy
+++ cdconfig/jenkinsfile.groovy
@@ -13,6 +13,29 @@ pipeline{
                 bat 'mvn --version'
             }
         }
+        stage ('Compile stage'){
+            steps {
+                withMaven(maven : 'MAVEN_HOME') {
+                    bat 'mvn clean compile'
+                }
+            }
+        }
+
+        stage ('Testing stage') {
+            steps {
+                withMaven(maven : 'MAVEN_HOME') {
+                    bat 'mvn test'
+                }
+            }
+        }
+
+        stage ('Deployment stage') {
+            steps {
+                withMaven(maven : 'MAVEN_HOME') {
+                    bat 'mvn install'
+                }
+            }
+        }
         stage('Build image'){
             steps{
                 echo "Build docker image"
