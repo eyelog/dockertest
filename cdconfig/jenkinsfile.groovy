3a9b6876b87d978ebcf19246c0f062261c7fd80e
eyelog <41166734+eyelog@users.noreply.github.com>
2019-02-24 01:49:21 +0300

    tap

--- cdconfig/jenkinsfile.groovy
+++ cdconfig/jenkinsfile.groovy
@@ -1,10 +1,25 @@
+def pipelineContext = [:]
+
 pipeline{
     agent any
+
+    environment {
+        DOCKER_IMAGE_TAG = "dockertest:build-${env.BUILD_ID}"
+    }
     stages{
-        stage('Test build'){
+        stage('Test environment'){
             steps{
                 echo 'Logcat Maven version'
                 bat 'mvn --version'
+            }
+        }
+        stage('Build image'){
+            steps{
+                echo "Build docker image"
+                script {
+                    dockerImage = docker.build("${env.DOCKER_IMAGE_TAG}",  '-f .cdconfig/dockerfile .')
+                    pipelineContext.dockerImage = dockerImage
+                }
             }
         }
     }
